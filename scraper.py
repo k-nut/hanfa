b7a534047151d7560d9523f72f138007321c27df
Knut Hühne <knut@k-nut.eu>
2015-10-31 13:26:40 +0100

    Save name of activity that is licensed, not nr

--- scraper.py
+++ scraper.py
@@ -53,10 +53,39 @@ def extract_details(part):
     saved["Website"] = " ".join(details_1[2].text.split(" ")[1:])
     if len(details_1) > 3:
         saved["BIC"] = details_1[3].text
-    saved["Approved activites"] = details_2[0].text.split(", ")
+    activities =  details_2[0].text.split(", ")
+    saved["Approved activites"] = [get_name_for_symbol(activity) for activity in activities]
     saved["Court register"] = details_2[1].find("a")["href"]
 
     return saved
+
+
+def get_name_for_symbol(symbol):
+    # taken from the bottom of http://www.hanfa.hr/en/registar/2
+    mapping = {
+        '1-01': 'reception and transmission of orders in relation to one or more financial instruments',
+        '1-02': 'execution of orders on behalf of clients',
+        '1-03': 'dealing on own account',
+        '1-04': 'portfolio management',
+        '1-05': 'investment advice',
+        '1-06': 'underwriting of financial instruments and/or placing of financial instruments on a firm commitment basis',
+        '1-07': 'placing of financial instruments without a firm commitment basis',
+        '1-08': 'operation of Multilateral Trading Facilities.',
+        '2-01': 'safekeeping and administration of financial instruments for the account of clients, including custodianship and related services such as cash/collateral management',
+        '2-02': 'granting credits or loans to an investor to allow him to carry out a transaction in one or more financial instruments, where the firm granting the credit or loan is involved in the transaction',
+        '2-03': 'advice to undertakings on capital structure, industrial strategy and related matters and advice and services relating to mergers and the purchase of undertakings',
+        '2-04': 'foreign exchange services where these are connected to the provision of investment services',
+        '2-05': 'investment research and financial analysis or other forms of general recommendation relating to transactions in financial instruments',
+        '2-06': 'services related to services under 1-06',
+        '2-07': 'investment services and activities as well as ancillary services related to the underlying of the derivatives where these investment services and activities are connected to the provision of investment or ancillary services.',
+        '3-01': 'organisation and implementation of the educational programme for capital market participants and users',
+        '3-02': 'insurance representation business',
+        '3-03': 'marketing of investment fund units',
+        '3-04': 'offering of pension schemes',
+        '3-05': 'other activities which, in Hanfa’s view, do not have any negative impact on the compliance with the Capital Market Act and relating regulations'
+    }
+    return mapping[symbol]
+
 
 if __name__ == "__main__":
     main()
